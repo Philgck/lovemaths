ff4f8dd4fd833e8ce476ca255dd1465b0a13c589
Philip Kershaw <pgckershaw@gmail.com>
2024-11-24 14:13:59 +0000

    Add: rungame function number generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,53 @@
-console.log('Hello, world!');
+// Run game
+
+document.addEventListener ("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+
+function runGame() {
+
+}
+// Check answer
+function checkAnswer() {
+
+}
+// Calculate answer
+function calculateCorrectAnswer() {
+
+}
+// Increment score
+function incrementScore() {
+
+}
+// Increment wrong answer
+function incrementWrongAnswer() {
+
+}
+// Display Addition question
+function displayAdditionQuestion() {
+
+}
+// Display Subtract question
+function displaySubtractQuestion() {
+
+}
+// Display Multiply question
+function displayMultiplyQuestion() {
+
+}
+    // Display Divide question
+function displayDivideQuestion() {
+
+}
